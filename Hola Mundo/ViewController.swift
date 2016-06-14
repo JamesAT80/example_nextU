fefd160dbcb01b6e7975639a7ced94a1e624b614
Jaime Torres <jaime.torres@pilautomation.com>
2016-06-14 12:54:44 -0500

    primeros cambios

--- Hola Mundo/ViewController.swift
+++ Hola Mundo/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func changeMessge(sender: AnyObject) {
+        messageLabel.text="Hola \(nameTextField.text!)"
+        view.endEditing(true)
+    }
 
+    //cambios comentarios
+    
 }
 
